530711788feae75b859e03c223683cb80811e894
Hannah <hannahreinl@gmail.com>
2025-09-08 10:17:02 -0400

    added favicon and react footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="footer-container">
       <span>by hannah Reinsel</span>
+      <span>Made with React ♡</span>
       <span>©2025</span>
     </div>
   );
